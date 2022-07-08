a7c6f0d3b0821c3a150934c9ba3822b6d6400f15
Harris <16213031+Labhatorian@users.noreply.github.com>
2022-07-08 23:36:46 +0200

    Fix thread not closing

--- app/src/main/java/powertoys/InfoDialog.java
+++ app/src/main/java/powertoys/InfoDialog.java
@@ -8,6 +8,8 @@ import javax.swing.text.SimpleAttributeSet;
 import javax.swing.text.StyleConstants;
 import javax.swing.text.StyledDocument;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.*;
 import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
@@ -19,14 +21,23 @@ public class InfoDialog extends JDialog {
     JScrollPane scrollPane = new JScrollPane(pane);
     private boolean holdUp = false;
     private boolean itWasntMe = false;
+    private movePane movePane = new movePane();
     public InfoDialog(JFrame parent) {
         //TODO Get version from somewhere
         setTitle("PowerToys Beta 1.0");
         setSize(dim);
         setPreferredSize(dim);
         setLocationRelativeTo(parent);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                //Tell the thread to bloody close
+                movePane.interrupt();
+            }
+        });
 
-        setLayout(new FlowLayout());
+                setLayout(new FlowLayout());
 
         JLabel title = new JLabel();
         title.setText("Powertoys Beta 1.0");
@@ -77,17 +88,14 @@ public class InfoDialog extends JDialog {
 
         setVisible(true);
 
-        //Prepare moving scrollbar and thread for it
+        //Prepare moving scrollbar and starts thread
         scrollPane.getViewport().addChangeListener(new ListenAdditionsScrolled());
-        movePane movePane = new movePane();
         movePane.start();
     }
     public class ListenAdditionsScrolled implements ChangeListener{
         public void stateChanged(ChangeEvent e){
             if(!itWasntMe) {
                 //Stops the scrollbar automatically moving when the user messes with it
-                scrollPane.revalidate();
-                repaint();
                 System.out.println("Konichuwa!");
                 holdUp = true;
             }
@@ -98,29 +106,33 @@ public class InfoDialog extends JDialog {
     //Using thread so this can run on its own without stopping the rest of the program
     //In a while loop so when the user messes with the scrollbar, it stops doing it and waits 10 seconds before restarting.
     private class movePane extends Thread {
-            public void run(){
-                //Get the scrollbar because that's an invidual component for some reason
-                JScrollBar vertical = scrollPane.getVerticalScrollBar();
-                while (!holdUp) {
-                    //Move the scrollbar and tell the listener it was not the user.
-                    System.out.println(vertical.getValue());
-                    itWasntMe = true;
-                    vertical.setValue(vertical.getValue()+1);
-                    itWasntMe = false;
+            public void run() {
+                try {
+                    //Get the scrollbar because that's an invidual component for some reason
+                    JScrollBar vertical = scrollPane.getVerticalScrollBar();
+                    while (!holdUp) {
+                        //Move the scrollbar and tell the listener it was not the user.
+                        System.out.println(vertical.getValue());
+                        itWasntMe = true;
+                        vertical.setValue(vertical.getValue() + 1);
+                        itWasntMe = false;
+                        try {
+                            TimeUnit.MILLISECONDS.sleep(200);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
                     try {
-                        TimeUnit.MILLISECONDS.sleep(200);
+                        TimeUnit.SECONDS.sleep(10);
                     } catch (InterruptedException e) {
                         throw new RuntimeException(e);
                     }
+                    holdUp = false;
+                    movePane = new movePane();
+                    movePane.start();
+                } catch (Exception e){
+                    //do nothing, most likely caused by the window closing
                 }
-                try {
-                    TimeUnit.SECONDS.sleep(10);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                holdUp = false;
-                movePane movePane = new movePane();
-                movePane.start();
             }
         }
     }
