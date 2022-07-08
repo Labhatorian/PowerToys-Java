2fcd483abe705067a86fc414ae588880b5fa82ab
Harris <16213031+Labhatorian@users.noreply.github.com>
2022-07-08 16:50:51 +0200

    Title and copyright added to InfoDialog.java

--- app/src/main/java/powertoys/InfoDialog.java
+++ app/src/main/java/powertoys/InfoDialog.java
@@ -9,14 +9,28 @@ import java.awt.*;
 import java.io.*;
 
 public class InfoDialog extends JDialog {
-
+    private Dimension dim = new Dimension(800, 420);
+    private Dimension dimPane = new Dimension(650,300);
     public InfoDialog(){
-        setTitle("PowerToys Beta 0.1");
-        setSize(800, 500);
+        //TODO Get version from somewhere
+        setTitle("PowerToys Beta 1.0");
+        setSize(dim);
+        setPreferredSize(dim);
         setLayout(new FlowLayout());
 
-        JTextPane pane = new JTextPane();
+        JLabel title = new JLabel();
+        title.setText("Powertoys Beta 1.0");
+        title.setFont(new Font(title.getName(), Font.PLAIN, 20));
 
+        JLabel copyright = new JLabel();
+        copyright.setText(" \u00A9 Labhatorian 2022");
+        copyright.setFont(new Font(title.getName(), Font.PLAIN, 15));
+
+        JTextPane pane = new JTextPane();
+        pane.setPreferredSize(dimPane);
+        pane.setEditable(false);
+
+        //Read the license from Resources
         BufferedReader in = null;
         try {
             ClassLoader classloader = Thread.currentThread().getContextClassLoader();
@@ -38,12 +52,26 @@ public class InfoDialog extends JDialog {
         } finally {
             try { in.close(); } catch (Exception ex) { }
         }
+        //
 
-        add(pane);
+        //Align Center
         StyledDocument doc = pane.getStyledDocument();
         SimpleAttributeSet centerAttribute = new SimpleAttributeSet();
         StyleConstants.setAlignment(centerAttribute, StyleConstants.ALIGN_CENTER);
         doc.setParagraphAttributes(0, doc.getLength(), centerAttribute, false);
+        //
+
+        JScrollPane scrollPane = new JScrollPane(pane);
+        pane.setCaretPosition(0); // Start at top when opening dialog
+
+        add(title);
+        add(scrollPane);
+        add(copyright);
+
         setVisible(true);
+    }
+
+    private void movePane(){
+        //TODO Move pane after short amount of no scrollpane movement downwards. Use timers
     }
 }
