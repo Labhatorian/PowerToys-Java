4a9ee36fe6d97a5e2928c64a5276997e2b69f7db
Harris <16213031+Labhatorian@users.noreply.github.com>
2022-07-09 14:21:40 +0200

    File crawler working in a simple form in FileSorter.java (Supposed to be FileRandomiser, oops!)

--- app/src/main/java/powertoys/FileSorter.java
+++ app/src/main/java/powertoys/FileSorter.java
@@ -1,16 +1,25 @@
 package powertoys;
 
+import org.checkerframework.checker.units.qual.A;
+
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+//TODO This is supposed to be the file sorter. Oops!
 public class FileSorter extends JPanel implements ActionListener {
     Button returnButton = new Button("Return");
     Button addFolder = new Button("Add folder");
+    Button randomiseButton = new Button("Randomise");
     JTable includedFolders;
     String[] columnNames = {"Included folder"};
     DefaultTableModel model = new DefaultTableModel(new Object[] { "Included Folders" }, 0);
@@ -27,8 +36,8 @@ public class FileSorter extends JPanel implements ActionListener {
         app.setTitle("PowerToys - FileSorter");
         addFolder.addActionListener(this);
         add(addFolder);
-
-
+        add(randomiseButton);
+        randomiseButton.addActionListener(this);
         includedFolders = new JTable(model);
         JScrollPane scrollPane = new JScrollPane(includedFolders);
         includedFolders.setFillsViewportHeight(true);
@@ -57,6 +66,28 @@ public class FileSorter extends JPanel implements ActionListener {
                 includedFolders.setModel(model);
                 revalidate();
                 repaint();
+            }
+        }
+        if (actionEvent.getSource() == randomiseButton) {
+            randomiseTime();
+        }
+    }
+
+    private void randomiseTime(){
+        ArrayList<String> files = new ArrayList<>();
+        List list = new List();
+        Stream<Path> allFiles = null;
+        ArrayList<Path> allFiles2 = new ArrayList<>();
+        for (String path:data
+             ) {
+            try {
+                Files.walk(Path.of(path)).filter(Files::isRegularFile).forEach(allFiles2::add);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            for (Path ongod:allFiles2
+                 ) {
+                System.out.println(ongod.toString());
             }
         }
     }
